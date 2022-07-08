97b37ca9aedb8363d8260febfc0866f48413ac10
BrunoAlz <bruno6821@gmail.com>
2022-07-08 14:57:10 -0300

    Criação do Faker para popular os Usuários

--- core/faker.py
+++ core/faker.py
@@ -2,30 +2,29 @@ from faker import Faker
 
 from django.contrib.auth.models import User
 
-def populate(n=0):
+def populate(tipo, quantidade=0):
+    """
+        Popula a tabela de Usuários com o número de Usuários passados
+    """
+    fake = Faker()
+    
+    if tipo == 'Usuarios':
+        for _ in range(quantidade):
+            fn = fake.first_name()
+            ln = fake.last_name()
+            username = f'{fn}_{ln}'.lower()
+            domain = fake.free_email_domain()
+            email = f'{fn}_{ln}@{domain}'.lower()
+            pass1 = '68210821Brn'
 
-    for _ in range(n):
-        fake = Faker()
-        fn = fake.first_name()
-        ln = fake.last_name()
-        username = f'{fn}_{ln}'
-        domain = fake.free_email_domain()
-        url = fake.url()
-        email = f'{fn}_{ln}@{domain}'
+            user = User.objects.create(
+                username=username,
+                password=pass1,
+                first_name=fn,
+                last_name=ln,
+                email=email                        
+                )
+            user.set_password(user.password)
+            user.save()
 
-        pass1 = '131288Brn'
-        pass2 = '131288Brn'
-
-        User.objects.get_or_create(
-            username=username,
-            first_name=fn,
-            last_name=ln,
-            email=email,            
-            )
-
-
-def avaliacao_populate(n=0):
-
-    for _ in range(n):
-        pass
         
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from core.faker import populate
+
 def core_index(request):
     template_name = 'core/core_index.html'
+    
+    # populate('Usuarios', 30)
 
     context = {
 

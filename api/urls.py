b59809af760fb3f39928243d83b8851e9102bf09
BrunoAlz <bruno6821@gmail.com>
2022-07-08 14:48:22 -0300

    Refactor da VIEW, usando generics api view
    Refatorando a view, para usar generic views.

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from .views import CursoAPIView, AvaliacaoAPIView, UsersAPIView
+from .views import (
+    AvaliacoesAPIView, CursoAPIView, 
+    AvaliacaoAPIView, CursosAPIView, 
+    UserAPIView, UsersAPIView
+    )
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    # Rotas de Listagem
+    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
     path('usuarios/', UsersAPIView.as_view(), name='users'),
+
+    # Rotas de Detalhes
+    path('cursos/<int:pk>', CursoAPIView.as_view(), name='curso'),
+    path('avaliacoes/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('usuarios/<int:pk>', UserAPIView.as_view(), name='user'),
+    
 ]
--- api/views.py
+++ api/views.py
@@ -1,66 +1,53 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from django.contrib.auth.models import User
+from rest_framework import generics
 from .models import Curso, Avaliacao
+from django.contrib.auth.models import User
 from .serializers import CursoSerializer, AvaliacaoSerializer, UsersSerializer
 
 
-class CursoAPIView(APIView):
-    # DESCRIÇÃO DA PÁGINA.
+class CursosAPIView(generics.ListCreateAPIView):
     """
     API de Cursos para usar no React / Flutter
     """
-
-    # implementa o metodo GET, que recebe a requisição
-    def get(self, request):
-        # Faz a Consulta para pegar os dados do banco e salva na variavel
-        cursos = Curso.objects.filter(ativo=1)
-        # Passa os dados da consulta para o Serializador, o many avisa que estamos passando todos os dados
-        serilizer = CursoSerializer(cursos, many=True)
-        # Retorna o response para a request, com os dados Serializados  
-        return Response(serilizer.data)
+    
+    # Faz a Consulta para pegar os dados do banco.
+    queryset = Curso.objects.filter(ativo=1)
+    # Chama o serealizador e passa os dados
+    serializer_class = CursoSerializer
 
 
-    # implementa o metodo POST, que recebe os dados na requisição
-    def post(self, request):
-        # O dados vem da requisição em 'request.data'
-        # a variavel 'data' que manda para o Serializador e manda pro bd
-        serializer = CursoSerializer(data=request.data)
-        # valida os dados, se passar OK, se não lança exceção
-        serializer.is_valid(raise_exception=True)
-        # salva no bd
-        serializer.save()
-        # retorna os dados Criados e o status HTTP
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    # Faz a Consulta para pegar os dados do banco.
+    queryset = Curso.objects.all()
+    # Chama o serealizador e passa os dados
+    serializer_class = CursoSerializer
 
 
-class AvaliacaoAPIView(APIView):
-
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.filter(ativo=1)
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    """
+    API de Avaliações para usar no React / Flutter
+    """
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
 
 
-class UsersAPIView(APIView):
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    # Faz a Consulta para pegar os dados do banco.
+    queryset = Curso.objects.all()
+    # Chama o serealizador e passa os dados
+    serializer_class = CursoSerializer
+
+
+class UsersAPIView(generics.ListCreateAPIView):
     """
     API de Usuários para usar no React / Flutter
     """
-    def get(self, request):
-        users = User.objects.filter(is_active=1)
-        serializer = UsersSerializer(users, many=True)
-        return Response(serializer.data)
+    queryset = User.objects.filter(is_active=1)
+    serializer_class = UsersSerializer
 
-    def post(self, request):
-        serializer = UsersSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class UserAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API de Usuários para usar no React / Flutter
+    """
+    queryset = User.objects.filter(is_active=1)
+    serializer_class = UsersSerializer

71ef0ea82862cbc62766a679b364a008a47ab647
BrunoAlz <bruno6821@gmail.com>
2022-07-11 13:58:54 -0300

    Adicionado Autenticação VIA TOKEN
    Adicionado Authenticação via token para consumir a API.

--- configurations/settings.py
+++ configurations/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'django_filters',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'accounts',
     'api',
@@ -125,7 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
